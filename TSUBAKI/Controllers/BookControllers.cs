8b7d6e1f26bef897b3c85cc826962da08ad1bc19
jtcuyugan <jamzymarietc@gmail.com>
2023-09-30 13:05:30 +0800

    Filled in Book Controllers
    Please refer to naming conventions

---
+++ TSUBAKI/Controllers/BookControllers.cs
@@ -0,0 +1,40 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Mvc;
+using TSUBAKI.Models.EntityManager;
+using TSUBAKI.Models.ViewModel;
+
+namespace TSUBAKI.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult Booking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(BookModel appointment)
+        {
+            if(ModelState.IsValid)
+            {
+                BookManager BM = new BookManager();
+                if(!BM.IsLoginNameExist(appointment.LoginName))
+                {
+                    BM.AddUserAccount(appointment);
+                    //FormsAuthentication.SetAuthCookie(user.FirstName, false);
+                    return RedirectToAction("", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Appointment schedule already taken.");
+            }
+            return View();
+        }
+            
+        [HttpGet]
+        public ActionResult GetAppointment()
+        {
+            var appointment = new BookManager().GetAllAppointments();
+            return View();
+        }
+    }
+}
